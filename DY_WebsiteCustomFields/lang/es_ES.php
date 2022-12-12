bdec5caa107d25c0189e6f96a91ff8f9b07d14fb
GetSimpleCMS-CE <119761508+GetSimpleCMS-CE@users.noreply.github.com>
2022-12-12 15:16:55 +0000

    Add files via upload

---
+++ DY_WebsiteCustomFields/lang/es_ES.php
@@ -0,0 +1,14 @@
+<?php
+$i18n = [
+    'CUSTOMFIELDS_DESCR' 			=> "Este complemento le permite especificar campos personalizados del sitio web.",
+    'FUNCTIONS_DESCR' 				=> "Se puede acceder a estos campos en sus temas:",
+    'GET_CUSTOM_FIELD_DESCR' 		=> "mostrará el valor del campo correspondiente.",
+    'RETURN_CUSTOM_FIELD_DESCR' 	=> "devolverá el valor del campo para su posterior procesamiento.",
+    'USAGE_DESCR' 					=> "A continuación puede configurar los campos personalizados del sitio web. Arrastre las filas para reordenar los campos. El valor predeterminado es el valor inicial del campo editado.",
+    'WEBSITECUSTOMFIELDS_EDIT' 		=> "Editar campos personalizados del sitio web",
+    'WEBSITECUSTOMFIELDS_CONFIGURE' => "Configurar",
+    'WEBSITECUSTOMFIELDS_TITLE' 	=> "Info de la Web 🗂️",
+    'WEBSITECUSTOMFIELDS_VIEW' 		=> "Config. campos personalizados de la Web 🗂️",
+    'NO_CUSTOMFIELDS' 				=> "¡Aún no se ha creado ningún campo personalizado del sitio web!",
+    'NO_CUSTOMFIELDS_PLUGIN' 		=> "¡El complemento <a href='http://get-simple.info/extend/plugin/i18n-custom-fields/100/' target='_blank'>I18N Custom Fields</a> debe estar instalado!"
+];
